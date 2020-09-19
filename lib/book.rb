4afab6707d59024f36837d81afb5e7ac95d9c7b3
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-19 01:15:08 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,42 @@
-
+class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+  
+  def initialize (title)    #on start
+    @title = title
+  end
+  
+  # def title                 #returns title
+  #   @title
+  # end
+  
+  # def author=(author)     #setter
+  #   @author = author
+  # end
+  
+  # def author              #getter
+  #   @author
+  # end
+  
+  # def page_count=(num)
+  #   @page_count = num
+  # end
+  
+  # def page_count
+  #   @page_count
+  # end
+  
+  # def genre=(genre)
+  #   @genre = genre
+  # end
+  
+  # def genre
+  #   @genre
+  # end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+  
+end
 
